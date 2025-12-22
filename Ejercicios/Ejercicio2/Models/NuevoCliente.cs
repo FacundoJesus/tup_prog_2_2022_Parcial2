8b827bf4aaef8f35f143c0e4c47c71afdfd95ea1
FacuJesus <facundojesus10@hotmail.com>
2025-12-21 23:17:39 -0300

    Avance

--- Ejercicios/Ejercicio2/Models/NuevoCliente.cs
+++ Ejercicios/Ejercicio2/Models/NuevoCliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicio2.Models
 {
-    internal class NuevoCliente
+    public class NuevoCliente
     {
     }
 }
--- Ejercicios/Ejercicio2/Models/Ticket.cs
+++ Ejercicios/Ejercicio2/Models/Ticket.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicio2.Models
 {
-    internal class Ticket
+    public class Ticket
     {
     }
 }
